7ee1a85ae678949b5d6950c63cf5873ee02044bf
rajeshkc-ai <rajesh.alard@gmail.com>
2026-05-30 00:41:25 +0530

    Update urs_logic.py

--- grains/wheat/urs_logic.py
+++ grains/wheat/urs_logic.py
@@ -100,12 +100,12 @@ def analyze_sample(cv_img, model):
 
         CLASS_THRESHOLDS = {
             'Foreign Matter': 0.40,
-            'Damage': 0.25,
+            'Damage': 0.15,
             'Shrivelled': 0.50,
             'Broken': 0.50,
             'Lustre Loss': 0.10,
             'Sound Grain': 0.40,
-            'Slightly Damage': 0.25,
+            'Slightly Damage': 0.15,
             'Ergoty Damage': 0.80
         }
 
